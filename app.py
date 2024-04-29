be4af0e8c93726ce46f2643e52c15f48df854d22
E1phiph0ros <candiceyao1016@gmail.com>
2024-04-28 22:32:24 -0400

    whatever

--- app.py
+++ app.py
@@ -6,29 +6,40 @@ import mysql.connector
 app = Flask(__name__)
 
 # Connect to the database
+mydb = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = "20011016",
+    database ="books"
+)
 
 # Retrieve all the categories from the database
-
+cursor = mydb.cursor()
+cursor.execute("SELECT * FROM category")
 # Fetch all the rows in a list of tuples called categories
-
-
+categories = cursor.fetchall()
 
 # set up routes
 @app.route('/')
 def home():
-    #Link to the index page.  Pass the categories as a parameter
-    return render_template()
+    # Link to the index page.  Pass the categories as a parameter
+    return render_template('index.html', categories=categories)
 
-@app.route('/category')
+
+@app.route('/category') # Store the categoryId passed as a URL parameter into a variable
 def category():
-    # Store the categoryId passed as a URL parameter into a variable
-
+    selectedCategory = request.args.get('categoryId', type=int)
     # Retrieve the books from the database that are associated with the selected categoryId
+    category_query = "SELECT * FROM book WHERE categoryId = %s"
+    cursor.execute(category_query, (selectedCategory,))
+    books = cursor.fetchall()
+    # Link to the category page, pass the selectedCategory, categories, and books as parameters
+    return render_template('category.html',
+                           selectedCategory=selectedCategory,
+                           categories=categories,
+                           books=books)
 
-    # Fetch all the rows in a list of tuples called books.
 
-    # Link to the category page.  Pass the selectedCategory, categories and books as parameters
-    return render_template()
 
 @app.errorhandler(Exception)
 def handle_error(e):
@@ -39,4 +50,4 @@ def handle_error(e):
 
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(debug=True, port=8080)
